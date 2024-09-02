e6dce109c912b0adbd5b00f1490a66533064db24
joaov-sha <joaovitor8832@gmail.com>
2024-09-02 12:31:18 -0300

    Criados os métodos de busca personalizados

--- src/main/java/br/edu/iftm/rastreamento/repository/PacoteRepository.java
+++ src/main/java/br/edu/iftm/rastreamento/repository/PacoteRepository.java
@@ -1,10 +1,19 @@
 package br.edu.iftm.rastreamento.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.iftm.rastreamento.model.Pacote;
+
 @Repository
 public interface PacoteRepository extends CrudRepository<Pacote, Long> {
 	
+    @Query("SELECT p FROM Pacote p WHERE p.status = :status")
+    List<Pacote> findByStatus(String status);
+
+    @Query("SELECT p FROM Pacote p WHERE p.destinatario = :destinatario")
+    List<Pacote> findByDestinatario(String destinatario);
 }
